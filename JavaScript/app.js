fd35f0fdcf2efad97daf7f1980a70821f99cb09c
Hassan Nadeem <hn178139@gmail.com>
2022-08-22 10:54:10 +0500

    com

---
+++ JavaScript/app.js
@@ -0,0 +1,21 @@
+/* Variable Declaration */
+//  var a = 12345;
+// var b;
+// var c = a+b;
+// console.log(c);
+
+// document.write(c)
+// console.log(12345+2);
+
+var x = "Hassan";
+var y = "Nadeem ";
+var z = x +" "+ y; // contatenation of string
+console.log(z);
+
+// alert(z);
+// alert("warning ! This page has cookies")
+var i= "my age is";
+var j = 15;
+console.log(i +""+ j)
+console.log("I am " + j + " years old")
+
