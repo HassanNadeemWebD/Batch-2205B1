7fbaba5bee40886f515254a21451d3cdeb054646
Hassan Nadeem <hn178139@gmail.com>
2022-08-26 10:50:08 +0500

    commit

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -7,15 +7,68 @@
 // document.write(c)
 // console.log(12345+2);
 
-var x = "Hassan";
-var y = "Nadeem ";
-var z = x +" "+ y; // contatenation of string
-console.log(z);
+// var x = "Hassan";
+// var y = "Nadeem ";
+// var z = x +" "+ y; // contatenation of string
+// console.log(z);
 
 // alert(z);
 // alert("warning ! This page has cookies")
-var i= "my age is";
-var j = 15;
-console.log(i +""+ j)
-console.log("I am " + j + " years old")
+// var i= "my age is";
+// var j = 15;
+// console.log(i +""+ j)
+// console.log("I am " + j + " years old")
+
+// Var, Let and Const Keywords in Variable Declaration
+
+// var a = 5;
+// console.log(a);
+let a = 5;
+console.log(a);
+a = 10;
+console.log(a)
+a = 9;
+console.log(a);
+{
+  let a = 6;
+  console.log(a);
+}
+
+// let a = 5; 
+
+const age = 18;
+
+console.log(age);
+
+// const gravity = 9.8;   
+// gravity = 10;    // error
+// console.log(gravity)  
+
+// let num1 = 6;
+// let num2 = 14;
+// let res = num2 % num1;
+// console.log(res);
+
+let i = 1 ;
+// console.log("example of pre-increment" + " " + ++i);
+// console.log("example of post-increment" + " " + i++);
+// console.log(i);
+
+// let ans = ++i + i++ + --i + i-- + i++ + i++ + ++i + --i;
+
+//         2   + 2  + 2 + 2 + 1 + 1 + 2 + 3  +2 + 1 
+
+let test = i-- + ++i - i++ + --i + ++i + i++ + i-- + ++i + ++i + --i + i--; 
+
+//         1    +  1  - 1   +  1  +  2  +  2  + 3   + 3   + 4   +  3  + 3
+
+// console.log(ans);
+console.log(test)
+
+
+
+
+
+
+
 
