a3a75f48f2b3863717dbe6e0017466cbea8bc421
HassanNadeemWebD <hn178139@gmail.com>
2022-09-28 10:12:22 +0500

    commit

---
+++ jquery/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+    $("#hideButton").click(function () {
+        // hide()   show()
+        // $("p").toggle()
+
+        // $("p").addClass('para');
+        // $("p").removeClass('para');
+
+    })
+
+    $("#slider").click(function () {
+
+        // $(".para").slideDown(2000 , function(){
+
+        //     $(".para").addClass('bg-warning');
+
+
+
+        // })
+        // $(".para").slideToggle(2000, function () {
+
+        //     $(".para").addClass('bg-warning');
+
+
+
+        // })
+        $(".para").fadeToggle(4000 , function(){
+
+            $(".para").addClass('bg-warning');
+    
+    
+    
+        })
+
+
+
+
+    });
+
+
+
+});
+
+
